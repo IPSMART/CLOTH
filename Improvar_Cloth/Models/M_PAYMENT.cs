2814a0c96e53d40da03199cceaba935a3ce223cc
Neha <IPSMART.ERP@gmail.com>
2020-08-27 17:35:00 +0530

    n

---
+++ Improvar_Cloth/Models/M_PAYMENT.cs
@@ -0,0 +1,38 @@
+namespace Improvar.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("M_PAYMENT")]
+    public partial class M_PAYMENT
+    {
+        public short? EMD_NO { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public string CLCD { get; set; }
+
+        [StringLength(1)]
+        public string DTAG { get; set; }
+
+        [StringLength(1)]
+        public string TTAG { get; set; }
+
+        [Key]
+        [StringLength(2)]
+        public string PYMTCD { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string PYMTNM { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public string GLCD { get; set; }
+
+        public long M_AUTONO { get; set; }
+    }
+}
