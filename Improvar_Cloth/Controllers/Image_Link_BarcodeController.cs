da65a3bd7fafc2facfb0f50cc9cd12c8c064457a
Mithun <IPSMART.ERP@gmail.com>
2020-10-03 12:00:32 +0530

    m

---
+++ Improvar_Cloth/Controllers/Image_Link_BarcodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Improvar.Controllers
+{
+    public class Image_Link_BarcodeController : Controller
+    {
+        // GET: Image_Link_Barcode
+        public ActionResult Image_Link_Barcode()
+        {
+            return View();
+        }
+    }
+}
---
+++ Improvar_Cloth/Controllers/Rep_BarcodePrintController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Improvar.Controllers
+{
+    public class Rep_BarcodePrintController : Controller
+    {
+        // GET: Rep_BarcodePrint
+        public ActionResult Rep_BarcodePrint()
+        {
+            return View();
+        }
+    }
+}
