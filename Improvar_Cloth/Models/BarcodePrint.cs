67005ea4b9d18306044d1fc02636eac17c7f148d
Neha <IPSMART.ERP@gmail.com>
2020-10-08 12:39:54 +0530

    n

--- Improvar_Cloth/Models/BarcodePrint.cs
+++ Improvar_Cloth/Models/BarcodePrint.cs
@@ -7,5 +7,20 @@ namespace Improvar.Models
 {
     public class BarcodePrint
     {
+        public short? EMD_NO { get; set; }
+        public string CLCD { get; set; }
+        public string DTAG { get; set; }
+        public string TTAG { get; set; }
+        public string AUTONO { get; set; }
+        public string TAXSLNO { get; set; }
+        public string BARNO { get; set; }
+        public string ITGRPNM { get; set; }
+        public string FABITNM { get; set; }
+        public string STYLENO { get; set; }
+        public string NOS { get; set; }
+        public string WPRATE { get; set; }
+        public string CPRATE { get; set; }
+        public string MTR { get; set; }
+        public bool Checked { get; set; }
     }
 }
