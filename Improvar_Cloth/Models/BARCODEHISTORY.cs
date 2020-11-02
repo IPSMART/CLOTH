f9f43a35ba51da94ea09753c0358cf59fae97672
Neha <IPSMART.ERP@gmail.com>
2020-11-02 14:50:03 +0530

    N

---
+++ Improvar_Cloth/Models/BARCODEHISTORY.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Improvar.Models
+{
+    public class BARCODEHISTORY
+    {
+        public string AUTONO { get; set; }
+        public short SLNO { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double? NOS { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double? OUTQNTY { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double? INQNTY { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double? RATE { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public double? DISCPER { get; set; }
+        public string PREFNO { get; set; }
+        public string SLNM { get; set; }
+        public string SLCD { get; set; }
+        public string DISTRICT { get; set; }
+        public string LOCNM { get; set; }
+        public string DOCDT { get; set; }
+        public string DOCNO { get; set; }
+        public string DOCNM { get; set; }
+        public bool Checked { get; set; }
+        
+
+    }
+}
---
+++ Improvar_Cloth/Models/BARCODEPRICE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Improvar.Models
+{
+    public class BARCODEPRICE
+    {
+        public short SLNO { get; set; }
+        public short EFFDT { get; set; }
+        public short PRCCD { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public short RATE { get; set; }
+        public bool Checked { get; set; }
+        
+    }
+}
