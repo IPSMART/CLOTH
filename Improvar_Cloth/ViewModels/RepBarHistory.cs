50b8c66952109ac1184b00d06682faae22df78c9
Neha <IPSMART.ERP@gmail.com>
2020-11-02 13:56:39 +0530

    n

--- Improvar_Cloth/ViewModels/RepBarHistory.cs
+++ Improvar_Cloth/ViewModels/RepBarHistory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Improvar.ViewModels
 {
@@ -21,11 +22,41 @@ namespace Improvar.ViewModels
         public string DOCCD { get; set; }
         public bool AskSlCd { get; set; }
         public string FDT { get; set; }
-        
         public string TDT { get; set; }
         public string SLCD { get; set; }
         public string SLNM { get; set; }
         public string DOCNM { get; set; }
         public string TEXTBOX6 { get; set; }
+        public string COLRCD { get; set; }
+        public string COLRNM { get; set; }
+        public string ITGRPCD { get; set; }
+        public string ITGRPNM { get; set; }
+        public string BARCODE { get; set; }
+        public string FABITCD { get; set; }
+        public string FABITNM { get; set; }
+        public string ITCD { get; set; }
+        public string ITNM { get; set; }
+        public string DESIGN { get; set; }
+        public string UOMCD { get; set; }
+        public string DISTRICT { get; set; }
+        public string GSTNO { get; set; }
+        public string PDESIGN { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public double TOTALIN { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double TOTALOUT { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double TOTINOUT { get; set; }
+        public string SIZECD { get; set; }
+        public string SIZENM { get; set; }
+        public List<BARCODEHISTORY> BARCODEHISTORY { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double? T_INQNTY { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double? T_OUTQNTY { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.000}", ApplyFormatInEditMode = true)]
+        public double? T_NOS { get; set; }
+
     }
 }
