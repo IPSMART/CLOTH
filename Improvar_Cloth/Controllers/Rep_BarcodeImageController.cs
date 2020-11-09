8158d3550e6d8fda72596844a243d3c9c515d70a
Mithun <IPSMART.ERP@gmail.com>
2020-11-09 10:34:35 +0530

    m

---
+++ Improvar_Cloth/Controllers/Rep_BarcodeImageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Improvar.Controllers
+{
+    public class Rep_BarcodeImageController : Controller
+    {
+        // GET: Rep_BarcodeImage
+        public ActionResult Rep_BarcodeImage()
+        {
+            return View();
+        }
+    }
+}
