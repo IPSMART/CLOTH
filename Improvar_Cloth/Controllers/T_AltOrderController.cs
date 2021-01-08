15dd7360109fa3c7bdf097e163981d829e677514
Mithun <IPSMART.ERP@gmail.com>
2021-01-08 15:45:39 +0530

    m

---
+++ Improvar_Cloth/Controllers/T_AltOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Improvar.Controllers
+{
+    public class T_AltOrderController : Controller
+    {
+        // GET: T_AltOrder
+        public ActionResult T_AltOrder()
+        {
+            return View();
+        }
+    }
+}
