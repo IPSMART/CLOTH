80520cbe0c04dba6b54a3ae9985ac148da9ba9e4
Tandrima <IPSMART.ERP@gmail.com>
2021-03-06 17:57:05 +0530

    T

---
+++ Improvar_Cloth/Controllers/Rep_SuplrAnlysController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Improvar.Controllers
+{
+    public class Rep_SuplrAnlysController : Controller
+    {
+        // GET: Rep_SuplrAnlys
+        public ActionResult Rep_SuplrAnlys()
+        {
+            return View();
+        }
+    }
+}
