3cc0fe557482c319b9a725a67996231118630d23
NEHA SINGH <nehasinghkol48@gmail.com>
2022-04-26 15:08:09 +0530

    n

--- Improvar_Cloth/Salesfunc.cs
+++ Improvar_Cloth/Salesfunc.cs
@@ -2513,7 +2513,7 @@ namespace Improvar
             sql += "sum(case a.stkdrcr when 'C' then a.qnty else a.qnty*-1 end) qnty " + Environment.NewLine;
             sql += "from " + scm + ".t_batchdtl a, " + scm + ".t_cntrl_hdr b, " + scm + ".t_txn c, " + scm + ".t_batchmst h, " + scm + ".t_bale g, " + Environment.NewLine;
             sql += scm + ".m_sitem d, " + scm + ".m_group e " + Environment.NewLine;
-            sql += sqlc + " and a.autono=autono=g.autono(+) and a.txnslno=g.slno(+) and g.autono is null and ";
+            sql += sqlc + " and a.autono=g.autono(+) and a.txnslno=g.slno(+) and g.autono is null and ";
             if (skipStkTrnf == true) sql += "c.doctag not in ('PB','OP','PD','JR') and "; else sql += "c.doctag not in ('PB','OP','PD','JR','SI','KH','TR') and " + Environment.NewLine;
             sql += "a.stkdrcr in ('D','C') " + Environment.NewLine;
             sql += "group by a.mtrljobcd, a.barno, h.itcd, a.mtrljobcd||h.itcd||a.barno " + Environment.NewLine;
